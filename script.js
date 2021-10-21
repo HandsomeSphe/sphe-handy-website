6272cabc9c9ce7234bc439c1945b46de0af55187
HandsomeSphe <siphelelehandsome@gmail.com>
2021-10-21 16:11:33 +0200

    more changes

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const popUp = document.querySelector(".popup");
-const whatsappBtn = document.querySelector(".whatsapp__btn");
-const closePopUp = document.querySelector(".close--popup");
+//Menu from burger icon
+const burger_icon = document.querySelector(".burger__icon");
+const smallscreens_menu = document.querySelector(".menu");
+const smallScreenRemove_btn = document.querySelector(".remove");
+const website = document.querySelector(".main");
+
+//Showing small screen menu
+burger_icon.addEventListener("click", function () {
+  smallscreens_menu.style.display = "block";
+  website.style.display = "none";
+});
+//Remove small screen menu
+smallScreenRemove_btn.addEventListener("click", function () {
+  smallscreens_menu.style.display = "none";
+  website.style.display = "";
+});
