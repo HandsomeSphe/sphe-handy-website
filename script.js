0bf2e0f620833abe25fa66818ab6daf47f3fbac4
HandsomeSphe <siphelelehandsome@gmail.com>
2021-10-24 06:01:25 +0200

    added javascript

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ smallScreenRemove_btn.addEventListener("click", function () {
   smallscreens_menu.style.display = "none";
   website.style.display = "";
 });
+//Implementing the smooth scroll
+///----------Method 1 -------------/////////
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
+///----------Method 2----------------//////////
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
+    console.log("LINK");
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+});
